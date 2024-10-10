4a95a8d675f491d4cceb217d063a7bdb4122d79a
mariam1070 <24assadm@centralfoundationboys.co.uk>
2024-10-10 13:15:22 +0000

    work

---
+++ ma.py
@@ -0,0 +1,8 @@
+number=int(input("enter any number"))
+if number<50:
+    print("failed")
+elif number==50:
+    print("equal")
+else:
+    print("passed")
+    
---
+++ yr12cs/ch1.py
@@ -0,0 +1,45 @@
+'''print("16")
+print("17/09/24")
+print(2+2)
+print(10%3)
+a=5#assigning a as 5
+b=10#assigning b as 10
+c=a+b#a90ssigning c as the output of a + b
+print(c)#output and print on screen c
+PI=3.1419#assigning PI as 3.1419
+radius=5#assigning radius as 5
+area = PI*radius*radius#assigning the areas as what PI is assigned to*what radius is aassigned to*what radius is assigned to
+print(area)#output and print on screen area'''
+
+'''
+number=int(input("enter any number"))
+if number>0:
+     print("positive")
+elif number==0:
+    print("its zero")
+else :
+    print("negative")'''
+ '''   '''
+'''number1=int(input("enter any number"))
+number2=int(input("enter any number"))
+if number1>number2:
+    print("greater")
+elif number2>number1:
+    print("lesser")
+else:
+    print("equal")'''
+'''number=int(input("enter any number"))
+if number<50:
+    print("failed")
+elif number==50:
+    print("equal")
+else:
+    print("passed")'''
+number1=int(input("enter any number"))
+number2=int(input("enter any number"))
+if number1%number2==0:
+    print("divisible")
+else:
+    print("not divisible")'''
+  
+
